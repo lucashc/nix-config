2cc052ad6a4d85a32d1489872acf36b7594a5cb7
Lucas Crijns <lucascrijns@gmail.com>
2021-02-08 22:10:11 +0100

    Add TRIM support

--- system-config/hardware-configuration.nix
+++ system-config/hardware-configuration.nix
@@ -16,25 +16,30 @@
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/627f1843-17a6-4a2a-862a-cbf8b7a0defa";
       fsType = "btrfs";
-      options = [ "subvol=root" "noatime" ];
+      options = [ "subvol=root" "noatime" "discard=async" ];
     };
 
-  boot.initrd.luks.devices."enc".device = "/dev/disk/by-uuid/71cd64f5-a4e3-4ce2-bf97-d80bfe7ed4bc";
+  boot.initrd.luks.devices."enc" = {
+    device = "/dev/disk/by-uuid/71cd64f5-a4e3-4ce2-bf97-d80bfe7ed4bc";
+    allowDiscards = true;
+  };
 
   fileSystems."/home" =
     { device = "/dev/disk/by-uuid/627f1843-17a6-4a2a-862a-cbf8b7a0defa";
       fsType = "btrfs";
-      options = [ "subvol=home" "compress=zstd" "noatime" ];
+      options = [ "subvol=home" "compress=zstd" "noatime" "discard=async" ];
     };
 
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/A4F7-81C6"; 
       fsType = "vfat";
+      options = [ "discard=async" ];
     };
 
   swapDevices = [
       { device = "/swap/swapfile";
-        size = 16000; }
+        size = 16000; 
+        }
     ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
